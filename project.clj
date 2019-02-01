f8d8a14c07f9b27e375281e507489ed7f9a4d3d8
Takashi AOKI <me@tak.sh>
2019-02-01 17:54:29 +0900

    version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-deconstruct-sigs "0.1.0-SNAPSHOT"
+(defproject clj-deconstruct-sigs "0.1.0"
   :description "deconstructSigs for Clojure"
   :url "https://github.com/chrovis/clj-deconstruct-sigs"
   :license {:name "Apache License, Version 2.0"
