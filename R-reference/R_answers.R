5875c62cf05d9fbfd4c1ee26ecb33bbff9753b1a
Takashi AOKI <me@tak.sh>
2019-04-11 15:16:22 +0900

    remove R reference files

--- R-reference/R_answers.R
+++
@@ -1,3 +0,0 @@
-library(deconstructSigs)
-dir.create("Routput")
-for (tumor_i in 1:nrow(randomly.generated.tumors)) {write.csv(whichSignatures(tumor.ref=randomly.generated.tumors[tumor_i,], signatures.ref=signatures.cosmic)[1], paste("Routput/answer-", tumor_i, ".csv" ,sep=""))}
